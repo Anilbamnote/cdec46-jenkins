7803a40f7301641a2411567c1268c9bb63126e00
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-02 12:24:36 +0530

    new-j-added

---
+++ pileline.groovy
@@ -0,0 +1,51 @@
+// pipeline {
+//     agent any
+//     stages {
+//         stage('git-pull') {
+//             steps {
+//                 echo "pull-sucess"
+//             }
+//         }
+//         stage('Build') {
+//             steps {
+//                 echo "build-sucess"
+//             }
+//         }
+//         stage('Test') {
+//             steps {
+//                 echo "test sucess"
+//             }
+//         }
+//         stage('Deploy') {
+//             steps {
+//                 echo "deploy sucess"
+//             }
+//         }
+//     }
+// }
+
+pipeline {
+    agent any
+    stages {
+        stage('git-pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "build-sucess"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "test sucess"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploy sucess"
+            }
+        }
+    }
+}
